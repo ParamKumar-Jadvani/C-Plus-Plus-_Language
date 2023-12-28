be7fd30311f2fa9b9f7e27d08dd71f96c76b21b2
I Am ParamKumar Jadvani <149695069+ParamKumar-Jadvani@users.noreply.github.com>
2023-12-28 14:51:35 +0530

    c++

--- Inheritance/Lecture-2/2.cpp
+++ Inheritance/Lecture-2/2.cpp
@@ -1,67 +1,73 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int getInt();
 
 class A
 {
-	protected : 
-		int a;
-	public :
-		A()
-		{
-			cout << "Enter A : ";
-			this->a = getInt();
-		}
+protected:
+	int a;
+
+public:
+	void setDataA()
+	{
+		cout << "Enter A : ";
+		this->a = getInt();
+	}
 };
 
 class B : public A
 {
-	protected : 
-		int b;
-	public :
-		B()
-		{
-			cout << "Enter B : ";
-			this->b = getInt();
-		}
+protected:
+	int b;
+
+public:
+	void setDataB()
+	{
+		cout << "Enter B : ";
+		this->b = getInt();
+	}
 };
 
 class C : public A
 {
-	protected : 
-		int c;
-	public :
-		C()
-		{
-			cout << "Enter C  :";
-			this->c = getInt();
-		}
+protected:
+	int c;
+
+public:
+	void setDataC()
+	{
+		cout << "Enter C  :";
+		this->c = getInt();
+	}
 };
 
-class D : public B,C
+class D : public B, C
 {
-	protected : 
-		int d;
-	public : 
-		D()
-		{
-			cout << "Enter D : ";
-			this->d = getInt();
-		}
-		
-		void sum()
-		{
-			cout << "Sum of all Number is : " << B::a << " + " << b << " + " << c << " + " << this->d << " = " << B::a+b+c+this->d;
-		}
+protected:
+	int d;
+
+public:
+	D()
+	{
+		B::setDataA();
+		B::setDataB();
+		C::setDataC();
+		cout << "Enter D : ";
+		this->d = getInt();
+	}
+
+	void sum()
+	{
+		cout << "Sum of all Number is : " << B::a << " + " << b << " + " << c << " + " << this->d << " = " << B::a + b + c + this->d;
+	}
 };
 
 int main()
 {
 	D s1;
-	
 	s1.sum();
-	
+
 	return 0;
 }
 
