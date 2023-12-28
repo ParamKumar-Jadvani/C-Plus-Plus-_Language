1c845398d8aa2a13b5074330a31430480c343c70
I Am ParamKumar Jadvani <149695069+ParamKumar-Jadvani@users.noreply.github.com>
2023-12-28 17:19:51 +0530

    c++

--- Data Abstraction/Lecture-1/1.cpp
+++ Data Abstraction/Lecture-1/1.cpp
@@ -1,34 +1,105 @@
-#include<iostream>
+#include <iostream>
+#include <string.h>
 using namespace std;
 
 int getInt();
-string getSting();
 void user();
 
 class Admin
 {
-	protected : 
-		string - can_terminate;
-		int manager_salary, employee_salary, total_staff, total_annual_revenue;
-		static string company_name;
-		
-		public :
-			void setData()
-			{
-				cout << "\t---Enter Data---" << endl
-					 << "Manager Salary : ";
-				this->manager_salary = getInt();
-				
-				cout << "Employee Salary : ";
-				this->employee_salary = getInt();
-				
-				cout << "Total Staff : ";
-				this->total_staff = getInt();
-				
-				cout << "Total Annual Revenue : ";
-				this->total_annual_revenue = getInt();
-				
-				cout << "When Terminate or Not : ";
-				this->can_terminate = getString();
-			}		
+private:
+    int total_Revenue;
+
+protected:
+    static string company_name;
+    int manager_salary, employee_salary, total_staff, can_terminate;
+
+public:
+    void setData()
+    {
+        cout << "\t---Enter Data---" << endl
+             << "Company Total Annual Revenue : ";
+        this->total_Revenue = getInt();
+
+        cout << "Manager Salary : ";
+        this->manager_salary = getInt();
+
+        cout << "Company TOtal Staff : ";
+        this->total_staff = getInt();
+
+        cout << "Employee Salary : ";
+        this->employee_salary = getInt();
+
+        cout << "Can Terminate : ";
+        this->can_terminate = getInt();
+    }
+
+    void myAccess()
+    {
+        cout << endl
+             << "\t---Company Data---" << endl
+             << "Company Total Revenue : " << this->total_Revenue << endl
+             << "Company Total Staff : " << this->total_staff << endl
+             << "Manager Salary : " << this->manager_salary << endl
+             << "Employee Salary : " << this->employee_salary << endl
+             << "Can Terminate : " << this->can_terminate << endl
+             << "Company Name : " << Admin::company_name << endl;
+    }
 };
+
+string Admin::company_name = "Google";
+
+class Manager : protected Admin
+{
+public:
+    void myAccess()
+    {
+        Admin::setData();
+        Admin::myAccess();
+
+        cout << endl
+             << "\t---Manager Data---" << endl
+             << "Manager Salary : " << this->manager_salary << endl
+             << "Total Staff : " << this->total_staff << endl
+             << "Employee Salary : " << this->employee_salary << endl
+             << "Can Terminate : " << this->can_terminate << endl
+             << "Company Name : " << this->Admin::company_name << endl;
+
+        manager_salary = 0;
+        total_staff = 0;
+        can_terminate = 0;
+    }
+};
+
+class Employee : private Manager
+{
+public:
+    void myAccess()
+    {
+        Manager::myAccess();
+        cout << endl
+             << "\t---Employee Salary---" << endl
+             << "Employee Salary : " << employee_salary << endl
+             << "Company Name : " << Admin::company_name << endl;
+    }
+};
+
+int main()
+{
+    user();
+    return 0;
+}
+
+void user()
+{
+    Employee e1;
+
+    e1.myAccess();
+}
+
+int getInt()
+{
+    int n;
+    cin >> n;
+    return n;
+}
